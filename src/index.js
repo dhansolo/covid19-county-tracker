95617f451247ef9c510486f0cf31a0c72b5b93a1
David Han <dhansolo@uw.edu>
2020-04-28 13:59:11 -0700

    Added world data and readjusted some elements

--- src/index.js
+++ src/index.js
@@ -43,6 +43,9 @@ class SearchAPI extends React.Component {
     handleSearchClick() {
         // console.log(this.state.county);
         // console.log(this.state.state);
+        let reformattedCounty = this.state.county[0].toUpperCase() + this.state.county.slice(1);
+        let reformattedState = this.state.state[0].toUpperCase() + this.state.state.slice(1);
+        this.setState({ county: reformattedCounty, state: reformattedState})
         axios({
             "method":"GET",
             "url":"https://covid-19-statistics.p.rapidapi.com/reports",
@@ -75,7 +78,7 @@ class SearchAPI extends React.Component {
     render() {
         let info;
         if(this.state.data) {
-            info = <Info confirmed={this.state.data.confirmed} deaths={this.state.data.deaths} county={this.state.county}/>;
+            info = <Info confirmed={this.state.data.confirmed} deaths={this.state.data.deaths} county={this.state.county} state={this.state.state}/>;
         }
         return (
             <div>
@@ -91,12 +94,64 @@ class SearchAPI extends React.Component {
 class Info extends React.Component {
     render() {
         let rate = (this.props.deaths/this.props.confirmed) * 100;
-        // console.log(rate.toFixed(3));
         return (
             <div>
-                <h2>Confirmed: {this.props.confirmed}</h2>
-                <h2>Deaths: {this.props.deaths}</h2>
-                <h2>Approximate Death Rate: {rate.toFixed(3)}%</h2>
+                <h2>Statistics for {this.props.county} County, {this.props.state}</h2>
+                <h3>Confirmed: {this.props.confirmed}</h3>
+                <h3>Deaths: {this.props.deaths}</h3>
+                <h3>Approximate Death Rate: {rate.toFixed(3)}%</h3>
+            </div>
+        )
+    }
+}
+
+class WorldData extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            data: null
+        }
+    }
+    componentDidMount() {
+        axios({
+            "method":"GET",
+            "url":"https://covid-19-statistics.p.rapidapi.com/reports/total",
+            "headers":{
+                "content-type":"application/octet-stream",
+                "x-rapidapi-host":"covid-19-statistics.p.rapidapi.com",
+                "x-rapidapi-key":"3de6fc79fbmshc411c445cdb1522p19aee5jsn38ed18b78689"
+            },"params":{
+                "date":current
+            }
+            })
+            .then((response)=>{
+              this.setState({ data: response.data.data});
+            })
+            .catch((error)=>{
+              console.log(error)
+            })
+    }
+    render() {
+        let data;
+        if(this.state.data) {
+            data = <DisplayWorldData data={this.state.data}/>
+        }
+        return (
+            <div>
+                {data}
+            </div>
+        )
+    }
+}
+
+class DisplayWorldData extends React.Component {
+    render() {
+        console.log(this.props.data);
+        let rate = (this.props.data.fatality_rate * 100);
+        return(
+            <div>
+                <h4>World Statistics</h4>
+                <h5>Confirmed: {this.props.data.confirmed} | Recovered: {this.props.data.recovered} | Deaths: {this.props.data.deaths} | Death Rate: {rate.toFixed(3)}%</h5>
             </div>
         )
     }
@@ -104,7 +159,11 @@ class Info extends React.Component {
 
 function App() {
     return (
-        <div><SearchAPI /></div>
+        <div>
+            <div><h1>Data as of {current}</h1></div>
+            <div><SearchAPI /></div>
+            <div><WorldData /></div>
+        </div>
     );
 }
 
