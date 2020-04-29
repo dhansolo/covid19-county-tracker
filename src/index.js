101cc1bac01abee9eba94e01ddbabb0bbd644d55
David Han <dhansolo@uw.edu>
2020-04-29 14:32:37 -0700

    Added loading animations, fixed inputs to better show field types, adjusted some more stylin and wording.

--- src/index.js
+++ src/index.js
@@ -6,8 +6,10 @@ import Grid from '@material-ui/core/Grid';
 import TextField from '@material-ui/core/TextField';
 import Select from '@material-ui/core/Select';
 import InputLabel from '@material-ui/core/InputLabel';
+import FormControl from '@material-ui/core/FormControl';
 import MenuItem from '@material-ui/core/MenuItem';
 import Button from '@material-ui/core/Button';
+import CircularProgress from '@material-ui/core/CircularProgress';
 
 // Create a date string in the format YYYY-MM-DD
 let date = new Date();
@@ -26,7 +28,8 @@ class SearchAPI extends React.Component {
         super(props);
         this.state = {
             county: "",
-            state: "Alabama",
+            state: "",
+            loading: false,
             data: null,
             notFound: false,
         }
@@ -36,13 +39,19 @@ class SearchAPI extends React.Component {
     }
 
     handleCountyChange(event) {
+        console.log(event.target.value);
         if(event.target.value) {
             this.setState({
                 county: event.target.value,
-                data: null
+                data: null,
+                loading: false
             });
-        } else {
-            console.log("please enter a county");
+        } else if(event.target.value === "") {
+            this.setState({
+                county: "",
+                data: null,
+                loading: false
+            });
         }
     }
 
@@ -59,14 +68,14 @@ class SearchAPI extends React.Component {
         // console.log(this.state.state);
         let reformattedCounty = this.state.county[0].toUpperCase() + this.state.county.slice(1);
         let reformattedState = this.state.state[0].toUpperCase() + this.state.state.slice(1);
-        this.setState({ county: reformattedCounty, state: reformattedState})
+        this.setState({ county: reformattedCounty, state: reformattedState, loading: true})
         axios({
             "method":"GET",
             "url":"https://covid-19-statistics.p.rapidapi.com/reports",
             "headers":{
                 "content-type":"application/octet-stream",
-                "x-rapidapi-host":"",
-                "x-rapidapi-key":""
+                "x-rapidapi-host":"covid-19-statistics.p.rapidapi.com",
+                "x-rapidapi-key":"3de6fc79fbmshc411c445cdb1522p19aee5jsn38ed18b78689"
             },
             "params":{
                 "region_province":this.state.state,
@@ -80,12 +89,12 @@ class SearchAPI extends React.Component {
         .then((response)=>{
             // console.log(response);
             if(response.data.data.length > 0) {
-                this.setState({ data: response.data.data[0].region.cities[0], notFound: false });
+                this.setState({ data: response.data.data[0].region.cities[0], notFound: false, loading: false });
                 confirmed = this.state.data.confirmed;
                 deaths = this.state.data.deaths;
                 // console.log(confirmed, deaths);
             } else {
-                this.setState({ data: null, notFound: true });
+                this.setState({ data: null, notFound: true, loading: false });
             }
         })
         .catch((error)=>{
@@ -96,6 +105,10 @@ class SearchAPI extends React.Component {
     render() {
         let info;
         let notfound;
+        let loadImage;
+        if(this.state.loading) {
+            loadImage = <LoadingScreen />
+        }
         if(this.state.data) {
             info = <Info confirmed={this.state.data.confirmed} deaths={this.state.data.deaths} county={this.state.county} state={this.state.state}/>;
         }
@@ -105,64 +118,69 @@ class SearchAPI extends React.Component {
         return (
             <div>
                 <div class="search">
-                    <TextField type="text" placeholder="County" value={this.state.county} onChange={this.handleCountyChange}></TextField>
-                    <Select type="text" value={this.state.state} onChange={this.handleStateChange}>
-                        <MenuItem value="" defaultValue disabled>Select State</MenuItem>
-                        <MenuItem value="Alabama">Alabama</MenuItem>
-                        <MenuItem value="Alaska">Alaska</MenuItem>
-                        <MenuItem value="Arizona">Arizona</MenuItem>
-                        <MenuItem value="Arkansas">Arkansas</MenuItem>
-                        <MenuItem value="California">California</MenuItem>
-                        <MenuItem value="Colorado">Colorado</MenuItem>
-                        <MenuItem value="Connecticut">Connecticut</MenuItem>
-                        <MenuItem value="Delaware">Delaware</MenuItem>
-                        <MenuItem value="Florida">Florida</MenuItem>
-                        <MenuItem value="Georgia">Georgia</MenuItem>
-                        <MenuItem value="Hawaii">Hawaii</MenuItem>
-                        <MenuItem value="Idaho">Idaho</MenuItem>
-                        <MenuItem value="Illinois">Illinois</MenuItem>
-                        <MenuItem value="Indiana">Indiana</MenuItem>
-                        <MenuItem value="Iowa">Iowa</MenuItem>
-                        <MenuItem value="Kansas">Kansas</MenuItem>
-                        <MenuItem value="Kentucky">Kentucky</MenuItem>
-                        <MenuItem value="Louisiana">Louisiana</MenuItem>
-                        <MenuItem value="Maine">Maine</MenuItem>
-                        <MenuItem value="Maryland">Maryland</MenuItem>
-                        <MenuItem value="Massachusetts">Massachusetts</MenuItem>
-                        <MenuItem value="Michigan">Michigan</MenuItem>
-                        <MenuItem value="Minnesota">Minnesota</MenuItem>
-                        <MenuItem value="Mississippi">Mississippi</MenuItem>
-                        <MenuItem value="Missouri">Missouri</MenuItem>
-                        <MenuItem value="Montana">Montana</MenuItem>
-                        <MenuItem value="Nebraska">Nebraska</MenuItem>
-                        <MenuItem value="Nevada">Nevada</MenuItem>
-                        <MenuItem value="New Hampshire">New Hampshire</MenuItem>
-                        <MenuItem value="New Jersey">New Jersey</MenuItem>
-                        <MenuItem value="New Mexico">New Mexico</MenuItem>
-                        <MenuItem value="New York">New York</MenuItem>
-                        <MenuItem value="North Carolina">North Carolina</MenuItem>
-                        <MenuItem value="North Dakota">North Dakota</MenuItem>
-                        <MenuItem value="Ohio">Ohio</MenuItem>
-                        <MenuItem value="Oklahoma">Oklahoma</MenuItem>
-                        <MenuItem value="Oregon">Oregon</MenuItem>
-                        <MenuItem value="Pennsylvania">Pennsylvania</MenuItem>
-                        <MenuItem value="Rhode Island">Rhode Island</MenuItem>
-                        <MenuItem value="South Carolina">South Carolina</MenuItem>
-                        <MenuItem value="South Dakota">South Dakota</MenuItem>
-                        <MenuItem value="Tennessee">Tennessee</MenuItem>
-                        <MenuItem value="Texas">Texas</MenuItem>
-                        <MenuItem value="Utah">Utah</MenuItem>
-                        <MenuItem value="Vermont">Vermont</MenuItem>
-                        <MenuItem value="Virginia">Virginia</MenuItem>
-                        <MenuItem value="Washington">Washington</MenuItem>
-                        <MenuItem value="West Virginia">West Virginia</MenuItem>
-                        <MenuItem value="Wisconsin">Wisconsin</MenuItem>
-                        <MenuItem value="Wyoming">Wyoming</MenuItem>
-                    </Select>
+                    <TextField id="search-county" type="text" label="County" value={this.state.county} onChange={this.handleCountyChange}></TextField>
+                    <FormControl id="search-state">
+                        <InputLabel>State</InputLabel>
+                        <Select type="text" value={this.state.state} onChange={this.handleStateChange}>
+                            <MenuItem value="Alabama">Alabama</MenuItem>
+                            <MenuItem value="Alaska">Alaska</MenuItem>
+                            <MenuItem value="Arizona">Arizona</MenuItem>
+                            <MenuItem value="Arkansas">Arkansas</MenuItem>
+                            <MenuItem value="California">California</MenuItem>
+                            <MenuItem value="Colorado">Colorado</MenuItem>
+                            <MenuItem value="Connecticut">Connecticut</MenuItem>
+                            <MenuItem value="Delaware">Delaware</MenuItem>
+                            <MenuItem value="Florida">Florida</MenuItem>
+                            <MenuItem value="Georgia">Georgia</MenuItem>
+                            <MenuItem value="Hawaii">Hawaii</MenuItem>
+                            <MenuItem value="Idaho">Idaho</MenuItem>
+                            <MenuItem value="Illinois">Illinois</MenuItem>
+                            <MenuItem value="Indiana">Indiana</MenuItem>
+                            <MenuItem value="Iowa">Iowa</MenuItem>
+                            <MenuItem value="Kansas">Kansas</MenuItem>
+                            <MenuItem value="Kentucky">Kentucky</MenuItem>
+                            <MenuItem value="Louisiana">Louisiana</MenuItem>
+                            <MenuItem value="Maine">Maine</MenuItem>
+                            <MenuItem value="Maryland">Maryland</MenuItem>
+                            <MenuItem value="Massachusetts">Massachusetts</MenuItem>
+                            <MenuItem value="Michigan">Michigan</MenuItem>
+                            <MenuItem value="Minnesota">Minnesota</MenuItem>
+                            <MenuItem value="Mississippi">Mississippi</MenuItem>
+                            <MenuItem value="Missouri">Missouri</MenuItem>
+                            <MenuItem value="Montana">Montana</MenuItem>
+                            <MenuItem value="Nebraska">Nebraska</MenuItem>
+                            <MenuItem value="Nevada">Nevada</MenuItem>
+                            <MenuItem value="New Hampshire">New Hampshire</MenuItem>
+                            <MenuItem value="New Jersey">New Jersey</MenuItem>
+                            <MenuItem value="New Mexico">New Mexico</MenuItem>
+                            <MenuItem value="New York">New York</MenuItem>
+                            <MenuItem value="North Carolina">North Carolina</MenuItem>
+                            <MenuItem value="North Dakota">North Dakota</MenuItem>
+                            <MenuItem value="Ohio">Ohio</MenuItem>
+                            <MenuItem value="Oklahoma">Oklahoma</MenuItem>
+                            <MenuItem value="Oregon">Oregon</MenuItem>
+                            <MenuItem value="Pennsylvania">Pennsylvania</MenuItem>
+                            <MenuItem value="Rhode Island">Rhode Island</MenuItem>
+                            <MenuItem value="South Carolina">South Carolina</MenuItem>
+                            <MenuItem value="South Dakota">South Dakota</MenuItem>
+                            <MenuItem value="Tennessee">Tennessee</MenuItem>
+                            <MenuItem value="Texas">Texas</MenuItem>
+                            <MenuItem value="Utah">Utah</MenuItem>
+                            <MenuItem value="Vermont">Vermont</MenuItem>
+                            <MenuItem value="Virginia">Virginia</MenuItem>
+                            <MenuItem value="Washington">Washington</MenuItem>
+                            <MenuItem value="West Virginia">West Virginia</MenuItem>
+                            <MenuItem value="Wisconsin">Wisconsin</MenuItem>
+                            <MenuItem value="Wyoming">Wyoming</MenuItem>
+                        </Select>
+                    </FormControl>
                 </div>
-                <div><Button variant="contained" color="primary" onClick={this.handleSearchClick}>Search</Button></div>
-                {info}
-                {notfound}
+                <Button id="search-button" variant="outlined" size="small" onClick={this.handleSearchClick}>Search</Button>
+                <div id="result">
+                    {loadImage}
+                    {info}
+                    {notfound}
+                </div>
             </div>
         )
     }
@@ -175,8 +193,8 @@ class Info extends React.Component {
             <div class="info">
                 <h3>Statistics for {this.props.county} County, {this.props.state}</h3>
                 <h4>Confirmed: {this.props.confirmed}</h4>
-                <h4>Deaths: {this.props.deaths}</h4>
-                <h4>Approximate Death Rate: {rate.toFixed(3)}%</h4>
+                <h4>Fatalities: {this.props.deaths}</h4>
+                <h4>Approximate Fatality Rate: {rate.toFixed(3)}%</h4>
             </div>
         )
     }
@@ -194,7 +212,8 @@ class WorldData extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            data: null
+            data: null,
+            loading: true
         }
     }
     componentDidMount() {
@@ -203,14 +222,14 @@ class WorldData extends React.Component {
             "url":"https://covid-19-statistics.p.rapidapi.com/reports/total",
             "headers":{
                 "content-type":"application/octet-stream",
-                "x-rapidapi-host":"covid-19-statistics.p.rapidapi.com",
-                "x-rapidapi-key":"3de6fc79fbmshc411c445cdb1522p19aee5jsn38ed18b78689"
+                "x-rapidapi-host":"",
+                "x-rapidapi-key":""
             },"params":{
                 "date":current
             }
             })
             .then((response)=>{
-              this.setState({ data: response.data.data});
+              this.setState({ data: response.data.data, loading: false});
             })
             .catch((error)=>{
               console.log(error)
@@ -218,13 +237,26 @@ class WorldData extends React.Component {
     }
     render() {
         let data;
+        let loadImage;
+        if(this.state.loading) {
+            loadImage = <LoadingScreen />
+        }
         if(this.state.data) {
             data = <DisplayWorldData data={this.state.data}/>
         }
         return (
             <div>
+                {loadImage}
                 {data}
             </div>
+        )
+    }
+}
+
+class LoadingScreen extends React.Component {
+    render() {
+        return (
+            <CircularProgress />
         )
     }
 }
@@ -236,7 +268,7 @@ class DisplayWorldData extends React.Component {
         return(
             <div>
                 <h4>World Statistics</h4>
-                <p>Confirmed: {this.props.data.confirmed} | Recovered: {this.props.data.recovered} | Deaths: {this.props.data.deaths} | Death Rate: {rate.toFixed(3)}%</p>
+                <p>Confirmed: {this.props.data.confirmed} | Recovered: {this.props.data.recovered} | Fatalities: {this.props.data.deaths} | Fatality Rate: {rate.toFixed(3)}%</p>
             </div>
         )
     }
@@ -244,15 +276,17 @@ class DisplayWorldData extends React.Component {
 
 function App() {
     return (
-        <Grid container justify="center">
-            <div class="main">
-                <div class="headers">
-                    <h1>COVID-19 County Tracker</h1>
-                    <h4>Data as of {current}</h4>
+        <Grid container>
+            <Grid item xs={12}>
+                <div class="main">
+                    <div class="headers">
+                        <h1>COVID-19 County Tracker</h1>
+                        <h4>Data as of {current}</h4>
+                    </div>
+                    <div class="search"><SearchAPI /></div>
+                    <footer class="world-data"><WorldData /></footer>
                 </div>
-                <div class="search"><SearchAPI /></div>
-                <footer class="world-data"><WorldData /></footer>
-            </div>
+            </Grid>
         </Grid>
     );
 }
