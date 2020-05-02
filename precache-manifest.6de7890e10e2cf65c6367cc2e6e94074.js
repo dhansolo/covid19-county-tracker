3728fb6def5627637795ceab852730e1d41e04f3
David Han <dhansolo@uw.edu>
2020-05-01 22:08:14 -0700

    Updates

---
+++ precache-manifest.6de7890e10e2cf65c6367cc2e6e94074.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e306c897d0e9269a2927c12151d13c3",
+    "url": "/covid19-county-tracker/index.html"
+  },
+  {
+    "revision": "eb8d21d3e38cd88ce89c",
+    "url": "/covid19-county-tracker/static/css/main.7a072cab.chunk.css"
+  },
+  {
+    "revision": "8806392c28e70362b9d4",
+    "url": "/covid19-county-tracker/static/js/2.ee533692.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/covid19-county-tracker/static/js/2.ee533692.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb8d21d3e38cd88ce89c",
+    "url": "/covid19-county-tracker/static/js/main.ff621559.chunk.js"
+  },
+  {
+    "revision": "e1698a11be7e5f797c0f",
+    "url": "/covid19-county-tracker/static/js/runtime-main.61edeb47.js"
+  }
+]);
